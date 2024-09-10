97efddeac3702bb191e75948efb1aca0008d7e64
Potter <aa4790139@gmail.com>
2024-09-10 21:23:24 +0800

    feat: integrate translate api

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,81 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const { translator, engines } = require("@yxw007/translate");
+const pkg = require("../package.json");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-translate-next" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-translate-next.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-translate-next!');
-	});
-
-	context.subscriptions.push(disposable);
+function initTranslator() {
+	translator.use(engines.google());
+	translator.use(engines.baidu({
+		appId: "",
+		secretKey: ""
+	}));
 }
 
-// This method is called when your extension is deactivated
+/**
+ * Extracts a text from the active document selection
+ *
+ * @param {vscode.TextDocument} document The current document
+ * @param {vscode.Selection} selection The current selection
+ * @returns {string} A text
+ */
+function getSelectedText(document: vscode.TextDocument, selection: vscode.Selection) {
+	const charRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+	return document.getText(charRange);
+}
+
+/**
+ * Generates the array of promises based on selections
+ *
+ * @param {Array.<vscode.Selection>} selections Array of selections
+ * @param {vscode.TextDocument} document The current document
+ * @param {string} selectedLanguage The current language
+ * @returns {Array.<Promise<TranslateRes>>}
+ */
+function getTranslationsPromiseArray(selections: Array<vscode.Selection>, document: vscode.TextDocument, selectedLanguage: string) {
+	return selections.map((selection: vscode.Selection) => {
+		const selectedText = getSelectedText(document, selection);
+		return getTranslationPromise(selectedText, selectedLanguage, selection);
+	});
+}
+
+/**
+ * Translates the selectedText to the selectedLanguage like a Promise
+ *
+ * @param {string} selectedText Text
+ * @param {string} selectedLanguage Language
+ * @param {vscode.Selection} selection Selection
+ * @returns {Promise.<TranslateRes>}
+ */
+function getTranslationPromise(selectedText: string, selectedLanguage: string, selection) {
+
+}
+
+function registerCommands(context: vscode.ExtensionContext) {
+	const translateText = vscode.commands.registerCommand('extension.translateText', () => {
+		//TODO: 如何没有设置目标语言，提示用户设置目标语言
+		const editor = vscode.window.activeTextEditor;
+		const { document, selections } = editor;
+		selections.map((selection) => {
+			const selectedText = getSelectedText(document, selection);
+			return getTranslationPromise(selectedText, selectedLanguage, selection);
+		});
+
+	});
+	context.subscriptions.push(translateText);
+
+	const setTargetLanguage = vscode.commands.registerCommand('extension.setTargetLanguage', () => {
+
+	});
+	context.subscriptions.push(setTargetLanguage);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	try {
+		initTranslator();
+		registerCommands(context);
+	} catch (error: any) {
+		vscode.window.showInformationMessage(`${pkg.name} extension active failed ! error:`, error);
+	}
+}
+
 export function deactivate() { }
