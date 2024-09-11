4395b90217f97920b263d1792e24e15eb7bf8e9a
Potter <aa4790139@gmail.com>
2024-09-11 09:56:35 +0800

    feat: integrate translate api

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,11 @@ function initTranslator() {
 	}));
 }
 
+type TranslateRes = {
+	selection: vscode.Selection;
+	translation: string
+}
+
 /**
  * Extracts a text from the active document selection
  *
@@ -31,10 +36,10 @@ function getSelectedText(document: vscode.TextDocument, selection: vscode.Select
  * @param {string} selectedLanguage The current language
  * @returns {Array.<Promise<TranslateRes>>}
  */
-function getTranslationsPromiseArray(selections: Array<vscode.Selection>, document: vscode.TextDocument, selectedLanguage: string) {
+function getTranslationsPromiseArray(editor: vscode.TextEditor, selections: Array<vscode.Selection>, document: vscode.TextDocument, selectedLanguage: string) {
 	return selections.map((selection: vscode.Selection) => {
 		const selectedText = getSelectedText(document, selection);
-		return getTranslationPromise(selectedText, selectedLanguage, selection);
+		return getTranslationPromise(editor, selectedText, selectedLanguage, selection);
 	});
 }
 
@@ -46,25 +51,62 @@ function getTranslationsPromiseArray(selections: Array<vscode.Selection>, docume
  * @param {vscode.Selection} selection Selection
  * @returns {Promise.<TranslateRes>}
  */
-function getTranslationPromise(selectedText: string, selectedLanguage: string, selection) {
+function getTranslationPromise(editor: vscode.TextEditor, selectedText: string, selectedLanguage: string, selection: vscode.Selection): Promise<TranslateRes> {
+	return new Promise((resolve, reject) => {
+		let decoration = vscode.window.createTextEditorDecorationType({
+			color: '#FF2D00',
+			backgroundColor: "transparent"
+		});
+		editor.setDecorations(decoration, [selection]);
 
+		translator.translate(selectedText, { to: selectedLanguage })
+			.then((res: any) => {
+				resolve({ selection, translation: res.text });
+				decoration.dispose();
+			})
+			.catch((e: any) => {
+				reject(e);
+				decoration.dispose();
+			});
+	});
 }
 
 function registerCommands(context: vscode.ExtensionContext) {
 	const translateText = vscode.commands.registerCommand('extension.translateText', () => {
 		//TODO: 如何没有设置目标语言，提示用户设置目标语言
 		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('No active text editor found !');
+			return;
+		}
+		//TODO: 从配置中获取目标语言
+		//TODO: 获取当前选中的翻译引擎
+		const selectedLanguage: string = vscode.workspace.getConfiguration('translate').get('targetLanguage') ?? "english";
 		const { document, selections } = editor;
-		selections.map((selection) => {
-			const selectedText = getSelectedText(document, selection);
-			return getTranslationPromise(selectedText, selectedLanguage, selection);
-		});
+
+		const translationsPromiseArray = getTranslationsPromiseArray(
+			editor,
+			Array.from(selections),
+			document,
+			selectedLanguage
+		);
+		Promise.all(translationsPromiseArray)
+			.then(function (results) {
+				editor.edit((builder) => {
+					results.forEach((r) => {
+						if (!!r.translation) {
+							builder.replace(r.selection, r.translation);
+						}
+					});
+				});
+			})
+			.catch((e) => vscode.window.showErrorMessage(e.message));
 
 	});
 	context.subscriptions.push(translateText);
 
 	const setTargetLanguage = vscode.commands.registerCommand('extension.setTargetLanguage', () => {
-
+		//TODO: 弹出一个选中列表，让用户选择目标语言
 	});
 	context.subscriptions.push(setTargetLanguage);
 }
