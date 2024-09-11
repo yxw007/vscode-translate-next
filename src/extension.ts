7c6c07f8d8dafe19da369f4656078573c6c04f4b
Potter <aa4790139@gmail.com>
2024-09-11 15:20:57 +0800

    feat: pass the most basic flow

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,28 @@
 import * as vscode from 'vscode';
 
-const { translator, engines } = require("@yxw007/translate");
+const { translator, engines, languageNames } = require("@yxw007/translate");
 const pkg = require("../package.json");
 
 function initTranslator() {
+	const azureConfig = vscode.workspace.getConfiguration('Translate-next.azure');
+	const amazonConfig = vscode.workspace.getConfiguration('Translate-next.amazon');
+	const baiduConfig = vscode.workspace.getConfiguration('Translate-next.baidu');
+
+	const get = (config: vscode.WorkspaceConfiguration, key: string): string => (config.get(key) ?? "");
+
 	translator.use(engines.google());
+	translator.use(engines.azure({
+		key: get(azureConfig, "key"),
+		region: get(azureConfig, "region")
+	}));
+	translator.use(engines.amazon({
+		region: get(amazonConfig, "region"),
+		key_id: get(amazonConfig, "key_id"),
+		access_key: get(amazonConfig, "access_key")
+	}));
 	translator.use(engines.baidu({
-		appId: "",
-		secretKey: ""
+		appId: get(baiduConfig, "app_id"),
+		secretKey: get(baiduConfig, "secret_key")
 	}));
 }
 
@@ -33,13 +48,13 @@ function getSelectedText(document: vscode.TextDocument, selection: vscode.Select
  *
  * @param {Array.<vscode.Selection>} selections Array of selections
  * @param {vscode.TextDocument} document The current document
- * @param {string} selectedLanguage The current language
+ * @param {string} targetLanguage The current language
  * @returns {Array.<Promise<TranslateRes>>}
  */
-function getTranslationsPromiseArray(editor: vscode.TextEditor, selections: Array<vscode.Selection>, document: vscode.TextDocument, selectedLanguage: string) {
+function getTranslationsPromiseArray(editor: vscode.TextEditor, selections: Array<vscode.Selection>, document: vscode.TextDocument, targetLanguage: string) {
 	return selections.map((selection: vscode.Selection) => {
 		const selectedText = getSelectedText(document, selection);
-		return getTranslationPromise(editor, selectedText, selectedLanguage, selection);
+		return getTranslationPromise(editor, selectedText, targetLanguage, selection);
 	});
 }
 
@@ -47,21 +62,21 @@ function getTranslationsPromiseArray(editor: vscode.TextEditor, selections: Arra
  * Translates the selectedText to the selectedLanguage like a Promise
  *
  * @param {string} selectedText Text
- * @param {string} selectedLanguage Language
+ * @param {string} targetLanguage Language
  * @param {vscode.Selection} selection Selection
  * @returns {Promise.<TranslateRes>}
  */
-function getTranslationPromise(editor: vscode.TextEditor, selectedText: string, selectedLanguage: string, selection: vscode.Selection): Promise<TranslateRes> {
+function getTranslationPromise(editor: vscode.TextEditor, selectedText: string, targetLanguage: string, selection: vscode.Selection): Promise<TranslateRes> {
 	return new Promise((resolve, reject) => {
 		let decoration = vscode.window.createTextEditorDecorationType({
 			color: '#FF2D00',
 			backgroundColor: "transparent"
 		});
 		editor.setDecorations(decoration, [selection]);
-
-		translator.translate(selectedText, { to: selectedLanguage })
-			.then((res: any) => {
-				resolve({ selection, translation: res.text });
+		const engine = vscode.workspace.getConfiguration('Translate-next').get("defaultEngine") ?? "google";
+		translator.translate(selectedText, { to: targetLanguage, engine })
+			.then((res: string[]) => {
+				resolve({ selection, translation: res[0] });
 				decoration.dispose();
 			})
 			.catch((e: any) => {
@@ -81,14 +96,15 @@ function registerCommands(context: vscode.ExtensionContext) {
 		}
 		//TODO: 从配置中获取目标语言
 		//TODO: 获取当前选中的翻译引擎
-		const selectedLanguage: string = vscode.workspace.getConfiguration('translate').get('targetLanguage') ?? "english";
+		const selectedLanguage: string = vscode.workspace.getConfiguration('translate-next').get('targetLanguage') ?? "english";
+		const targetLanguage = Object.keys(languageNames).find((key) => selectedLanguage.toLowerCase().indexOf(key) >= 0) ?? "english";
 		const { document, selections } = editor;
 
 		const translationsPromiseArray = getTranslationsPromiseArray(
 			editor,
 			Array.from(selections),
 			document,
-			selectedLanguage
+			targetLanguage
 		);
 		Promise.all(translationsPromiseArray)
 			.then(function (results) {
